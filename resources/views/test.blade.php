efe64da3a6c817792d3eb6d888298ed6d6d59962
Samuel <samiayalneh@gmail.com>
2023-07-14 14:39:43 +0300

    commit file

---
+++ resources/views/test.blade.php
@@ -0,0 +1,9 @@
+<?php
+
+echo  "$name.<br>";
+echo  "$name2";
+
+
+
+
+?>
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/test', function () {
+    return "Testing Laravel Project";
+});
+Route::view('/welcome', 'test', ['name' => 'Taylor','name2' => 'Sami']);
+Route::resource('tests', TestController::class);
